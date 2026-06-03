25e9cc2e8a1c80c5a6e7796f81020e36f560fb8e
Funmi77 <vrcreativeagency@gmail.com>
2026-06-03 16:41:48 +0100

    Built Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Symptoms from "./components/Symptoms";
 import Products from "./components/Products";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Symptoms />
       <Products />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Built by Funmi Fagbemi</p>
+      <p>Frontend Developer Portfolio Project</p>
+
+      <div className="links">
+        <a href="https://github.com/Funmi77">GitHub</a>
+        <a href="www.linkedin.com/in/funmilayo-fagbemi">LinkedIn</a>
+        <a href="https://portfoilo-page.netlify.app/">Portfolio</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
